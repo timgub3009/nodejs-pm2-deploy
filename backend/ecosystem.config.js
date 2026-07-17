743b3114429ac6f8dcbdfa54ebbdfed618c23a77
Gubeidulin Timur <timur3009@yandex.ru>
2026-07-17 18:04:29 +0300

    конфиги добавил

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,44 @@
+require('dotenv').config({ path: '../.env.deploy' });
+
+const {
+  DEPLOY_USER,
+  DEPLOY_HOST,
+  DEPLOY_PATH,
+  DEPLOY_REF,
+  DEPLOY_REPO,
+} = process.env;
+
+module.exports = {
+  apps: [
+    {
+      name: 'mesto-backend',
+      script: './dist/app.js',
+      cwd: `${DEPLOY_PATH}/current/backend`,
+      autorestart: true,
+      restart_delay: 3000,
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+
+      'pre-deploy': `scp .env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/shared/.env`,
+
+      'post-deploy': [
+        'cd backend',
+        'npm ci',
+        'npm run build',
+        `cp ${DEPLOY_PATH}/shared/.env .env`,
+        'pm2 startOrRestart ecosystem.config.js --env production',
+      ].join(' && '),
+    },
+  },
+};
