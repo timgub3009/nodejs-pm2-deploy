18425f077b988d32dc7621376feaf91df4d16108
Gubeidulin Timur <timur3009@yandex.ru>
2026-07-17 17:12:01 +0300

    initial edits

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -16,6 +16,13 @@ mongoose.connect(DB_ADDRESS);
 // Только для локальных тестов. Не используйте это в продакшене
 // app.use(cors())
 app.use(express.json());
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(routes);
